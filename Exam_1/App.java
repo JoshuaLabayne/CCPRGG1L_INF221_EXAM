3264fb0d4a60c20ac4829d7c1dd4bce5d345ad58
Joshua Caleb Labayne <111472866+JoshuaLabayne@users.noreply.github.com>
2022-09-05 10:11:07 +0800

    Add files via upload

---
+++ Exam_1/App.java
@@ -0,0 +1,25 @@
+public class App {
+    public static void main(String[] args) {
+
+      
+      
+
+        
+        
+     Student student = new Student();
+
+    student.surname = "Labayne";
+    student.firstName = "Joshua";
+    student.middleInitial = 'D';
+    student.dateOfBirth = 05/17/2003;
+    student.studentNumber = 2022102019;
+    student.studentEmailAddress = "Labaynejd1@student.national-u.edu.ph";
+    student.iAmAwesome = true;
+    
+    student.sayMyStudentNumber();
+    student.sayMyEmailAddress();
+    student.sayIamAwesome();
+
+}
+    
+}
---
+++ Exam_1/Student.java
@@ -0,0 +1,26 @@
+
+public class Student {
+    String surname;
+    String firstName;
+    char middleInitial;
+    int dateOfBirth;
+    int studentNumber;
+    String studentEmailAddress;
+    Boolean iAmAwesome;
+      
+ public void sayMyStudentNumber () {
+    System.out.println("Hello my student number is " + studentNumber);
+ 
+  }
+     public void sayMyEmailAddress() {
+    System.out.println("Hello my Email address is " + studentEmailAddress);
+
+ 
+
+     }  
+       public void sayIamAwesome() {
+       System.out.println("Hello I am Awesome " + iAmAwesome);  
+    
+       }
+    
+}
